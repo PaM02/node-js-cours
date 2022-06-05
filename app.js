8319e62c8b26865b023b048d873669a482a380a5
papa matar fall <matar026@gmail.com>
2022-06-05 19:40:50 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
 
 mongoose.connect('mongodb+srv://paf:bPfjzoRB4idlJpky@cluster0.dulnduu.mongodb.net/?retryWrites=true&w=majority', {
@@ -24,73 +24,6 @@ app.use((req, res, next) => {
 /* post */
 /* ce middleware nous permet de recuperer le corps json*/
 app.use(express.json());
-app.post('/api/stuff', (req, res, next) => {
+app.use('/api/stuff', stuffRoutes);
 
-    // supprimer la requete _id
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-/* app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
-    });
-}); */
-
-/* end post */
-
-/* delete */
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-/* put pour modifier */
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-/* request get */
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
-
-// app.get('/api/stuff', (req, res, next) => {
-//     const stuff = [{
-//             _id: 'oeihfzeoi',
-//             title: 'Mon premier objet',
-//             description: 'Les infos de mon premier objet',
-//             imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-//             price: 4900,
-//             userId: 'qsomihvqios',
-//         },
-//         {
-//             _id: 'oeihfzeomoihi',
-//             title: 'Mon deuxième objet',
-//             description: 'Les infos de mon deuxième objet',
-//             imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-//             price: 2900,
-//             userId: 'qsomihvqios',
-//         },
-//     ];
-//     res.status(200).json(stuff);
-// });
 module.exports = app;
